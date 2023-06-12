7f9a1baf96054489a525ad37c774515ca27a178f
CleidsonGoes <jorgegoes4@gmail.com>
2023-06-11 21:52:36 -0300

    requisito 03 em desenv.

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,22 @@
+import { useState } from 'react';
 import './App.css';
 import Form from './components/Form';
 
 function App() {
+  const [exibitionButtonPassword, setExibitionButtonPassword] = useState(false);
+  const [exibitionForm, setExibitionForm] = useState(false);
+
+  const handlerExibitionComponent = () => {
+    setExibitionForm(true);
+  };
+
   return (
     <>
       <h1>Gerenciador de senhas</h1>
-      <Form />
+      {
+        exibitionForm ? <Form />
+          : <button onClick={ handlerExibitionComponent }>Cadastrar nova senha</button>
+      }
     </>
   );
 }
